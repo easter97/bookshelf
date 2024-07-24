eaa9993b1e057b01645fed6833028761361a899d
Amanda Easter <me@amandaeaster.com>
2024-07-24 15:59:06 -0500

    files

---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module'
+import { BrowserModule } from '@angular/platform-browser';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    BrowserModule,
+    CommonModule,
+    HttpClientModule,
+    AppRoutingModule,
+  ]
+})
+export class AppModule { }
